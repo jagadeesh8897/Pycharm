e83cd3b9cc57af3b6f8da6055dc8da3e5a287249
Jagadeesh <224g1a0530@srit.ac.in>
2025-07-10 06:49:12 +0530

    removed file

--- CountWords.py
+++
@@ -1,8 +0,0 @@
-def count_words(flnm):
-    with open(flnm,'r') as f:
-        content=f.read()
-        words=content.split()
-        cnt=len(words)
-        return f"No of words in a file: {cnt}"
-fi=('file')
-print(count_words(fi))
